c95961a07cace0ff5ec8d0c01c0ce6eaaed16c63
LanceNoble <ltnlikespie@gmail.com>
2025-07-25 20:13:03 -0400

    Display web page

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace WindowsFormsApp1
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
 {
     partial class Form1
     {
@@ -28,21 +33,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Margin = new System.Windows.Forms.Padding(0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 450);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private WebView2 webView;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,33 +1,109 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using Microsoft.Web.WebView2.Core;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [Flags]
+        enum SendMessageTimeoutFlags : uint
         {
-            InitializeComponent();
-
-            //WindowState = FormWindowState.Maximized;
-            FormBorderStyle = FormBorderStyle.None;
-            //Size = new Size(500, 500);
-            //Location = new System.Drawing.Point(100, 0);
-            Size = Screen.PrimaryScreen.Bounds.Size;
-
-
+            SMTO_NORMAL = 0x0000,
+            SMTO_BLOCK = 0x0001,
+            SMTO_ABORTIFHUNG = 0x0002,
+            SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        [DllImport("User32.dll")]
+        static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr SendMessageTimeout(
+            IntPtr hWnd,
+            UInt32 Msg,
+            IntPtr wParam,
+            IntPtr lParam,
+            SendMessageTimeoutFlags fuFlags,
+            UInt32 uTimeout,
+            out IntPtr lpdwResult);
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern Boolean EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr FindWindowEx(
+            IntPtr hwndParent,
+            IntPtr hwndChildAfter,
+            String lpszClass,
+            IntPtr lpszWindow);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("User32.dll")]
+        static extern Boolean SystemParametersInfo(UInt32 uAction, UInt32 uParam, String lpvParam, UInt32 fuWinIni);
+
+        public Form1()
         {
-            
+            // TODO: Only send progman message if workerw window can't be found
+            IntPtr progman = FindWindow("Progman", null);
+            SendMessageTimeout(
+                FindWindow("Progman", null),
+                0x052C,
+                IntPtr.Zero,
+                IntPtr.Zero,
+                SendMessageTimeoutFlags.SMTO_NORMAL,
+                1000,
+                out _);
+            IntPtr workerw = IntPtr.Zero;
+            Boolean enumResult = EnumWindows(new EnumWindowsProc((hwnd, lParam) =>
+            {
+                IntPtr searchResult = FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", IntPtr.Zero);
+                if (searchResult != IntPtr.Zero)
+                {
+                    workerw = FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", IntPtr.Zero);
+                }
+                return true;
+            }), IntPtr.Zero);
+
+            SetParent(this.Handle, workerw);
+            InitializeComponent();
+            this.Location = new Point(0, 0);
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.CoreWebView2.DOMContentLoaded += async (foo, args) =>
+            {
+                String path = File.ReadAllText(".\\web\\index.js");
+                await webView.CoreWebView2.ExecuteScriptAsync(path);
+            };
+            webView.CoreWebView2.Navigate("https://www.shadertoy.com/view/3csSWB");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+            const int SPI_SETDESKWALLPAPER = 20;
+            const int SPIF_UPDATEINIFILE = 0x01;
+            const int SPIF_SENDWININICHANGE = 0x02;
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                "C:\\Users\\Lance\\Desktop\\2560x1440-48404.png",
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
         }
     }
 }
